4daa97066c231421eda5d976b77f51ed2f4d235e
kieranbentley1999 <kieranbentley1@yahoo.co.uk>
2026-08-02 18:49:14 +0100

    Fix all character image paths in database.js

--- database.js
+++ database.js
@@ -1,38 +1,52 @@
+// database.js - Phase 1 Common Launch Roster
+
 const brainrotCharacters = [
-  { name: "Noobini Pizzanini", rarity: "secret", image: "brainrots/noobini_pizzanini.png" },
-  { name: "Holy Arepa", rarity: "secret", image: "brainrots/holy_arepa.png" },
-  { name: "Lirili Larila", rarity: "secret", image: "brainrots/lirili_larila.png" },
-  { name: "Tim Cheese", rarity: "secret", image: "brainrots/tim_cheese.png" },
-  { name: "Flurifura", rarity: "secret", image: "brainrots/flurifura.png" },
-  { name: "Los Tungtungtungcitos", rarity: "secret", image: "brainrots/los_tungtungtungcitos.png" },
-  { name: "Fortunu and Cashuru", rarity: "secret", image: "" },
-  { name: "Los Amigos", rarity: "secret", image: "" },
-  { name: "La Secret Combinasion", rarity: "secret", image: "" },
-  { name: "Foxini Lanternini", rarity: "secret", image: "" },
-  { name: "Fragrama and Chocrama", rarity: "secret", image: "" },
-  { name: "La Casa Boo", rarity: "secret", image: "" },
-  { name: "Griffin", rarity: "secret", image: "" },
-  { name: "Hydra Dragon Cannelloni", rarity: "secret", image: "" },
-  { name: "Dragon Gingerini", rarity: "secret", image: "" },
-  { name: "Love Love Bear", rarity: "secret", image: "" },
-  { name: "Cerberus", rarity: "secret", image: "" },
-  { name: "Celestial Pegasus", rarity: "secret", image: "" },
-  { name: "Capitano Moby", rarity: "secret", image: "" },
-  { name: "Bombardiro Crocodilo", rarity: "secret", image: "" },
-  { name: "Boneca Amaldicoada", rarity: "secret", image: "" },
-  { name: "Chimpenzini Bananini", rarity: "secret", image: "" },
-  { name: "Gorilloni Ananasoni", rarity: "secret", image: "" },
-  { name: "Elefanti Melonini", rarity: "secret", image: "" },
-  { name: "Calamari Fritti", rarity: "secret", image: "" },
-  { name: "Spaghetti T-Rex", rarity: "secret", image: "" },
-  { name: "Tung Tung Sah", rarity: "secret", image: "" },
-  { name: "Mamma Mia Pizzerio", rarity: "secret", image: "" },
+  // ====================
+  // 🟢 PHASE 1: COMMON ROTS
+  // ====================
+  { name: "Noobini Pizzanini", rarity: "common", reward: 1, image: "brainrots/noobini_pizzanini.png" },
+  { name: "Holy Arepa", rarity: "common", reward: 1, image: "brainrots/holy_arepa.png" },
+  { name: "Lirili Larila", rarity: "common", reward: 1, image: "brainrots/lirili_larila.png" },
+  { name: "Tim Cheese", rarity: "common", reward: 1, image: "brainrots/tim_cheese.png" },
+  { name: "Flurifura", rarity: "common", reward: 1, image: "brainrots/flurifura.png" },
+  { name: "Los Tungtungtungcitos", rarity: "common", reward: 1, image: "brainrots/los_tungtungtungcitos.png" },
+  { name: "Talpa Di Fero", rarity: "common", reward: 1, image: "brainrots/talpa.png" },
+  { name: "Svinina Bombardino", rarity: "common", reward: 1, image: "brainrots/svinina.png" },
+  { name: "Pipi Kiwi", rarity: "common", reward: 1, image: "brainrots/pipi_kiwi.png" },
+  { name: "Pipi Corni", rarity: "common", reward: 1, image: "brainrots/pipi_corni.png" },
+  { name: "Racooni Jandelini", rarity: "common", reward: 1, image: "brainrots/racooni.png" },
+
+  // ====================
+  // 🔒 FUTURE UPDATES (SECRETS)
+  // ====================
+  { name: "Fortunu and Cashuru", rarity: "secret", reward: 1, image: "" },
+  { name: "Los Amigos", rarity: "secret", reward: 1, image: "" },
+  { name: "La Secret Combinasion", rarity: "secret", reward: 1, image: "" },
+  { name: "Foxini Lanternini", rarity: "secret", reward: 1, image: "" },
+  { name: "Fragrama and Chocrama", rarity: "secret", reward: 1, image: "" },
+  { name: "La Casa Boo", rarity: "secret", reward: 1, image: "" },
+  { name: "Griffin", rarity: "secret", reward: 1, image: "" },
+  { name: "Hydra Dragon Cannelloni", rarity: "secret", reward: 1, image: "" },
+  { name: "Dragon Gingerini", rarity: "secret", reward: 1, image: "" },
+  { name: "Love Love Bear", rarity: "secret", reward: 1, image: "" },
+  { name: "Cerberus", rarity: "secret", reward: 1, image: "" },
+  { name: "Celestial Pegasus", rarity: "secret", reward: 1, image: "" },
+  { name: "Capitano Moby", rarity: "secret", reward: 1, image: "" },
+  { name: "Bombardiro Crocodilo", rarity: "secret", reward: 1, image: "" },
+  { name: "Boneca Amaldicoada", rarity: "secret", reward: 1, image: "" },
+  { name: "Chimpenzini Bananini", rarity: "secret", reward: 1, image: "" },
+  { name: "Gorilloni Ananasoni", rarity: "secret", reward: 1, image: "" },
+  { name: "Elefanti Melonini", rarity: "secret", reward: 1, image: "" },
+  { name: "Calamari Fritti", rarity: "secret", reward: 1, image: "" },
+  { name: "Spaghetti T-Rex", rarity: "secret", reward: 1, image: "" },
+  { name: "Tung Tung Sah", rarity: "secret", reward: 1, image: "" },
+  { name: "Mamma Mia Pizzerio", rarity: "secret", reward: 1, image: "" },
   
   // ====================
   // ✨ OG
   // ====================
-  { name: "Strawberry Elephant", rarity: "og", image: "" },
-  { name: "Skibidi Toilet", rarity: "og", image: "" },
-  { name: "Headless Horseman", rarity: "og", image: "" },
-  { name: "Meowl", rarity: "og", image: "" }
+  { name: "Strawberry Elephant", rarity: "og", reward: 1, image: "" },
+  { name: "Skibidi Toilet", rarity: "og", reward: 1, image: "" },
+  { name: "Headless Horseman", rarity: "og", reward: 1, image: "" },
+  { name: "Meowl", rarity: "og", reward: 1, image: "" }
 ];
